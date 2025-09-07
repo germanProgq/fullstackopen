3dc8ba8ff13bcf8a7cac469d6a15485f40f2c2f5
German <157655511+germanProgq@users.noreply.github.com>
2025-09-07 19:26:42 +0300

    courseinfo step 9

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,24 +1,19 @@
-import React from "react";
-
 const Header = ({ name }) => <h1>{name}</h1>
 const Description = ({ text, exercises }) => <p>{text} {exercises}</p>
 const Sum = ({ parts }) => {
-    let sum = 0;
-    parts.forEach(part => {
-        sum += part.exercises;        
-    });
-    return <p style={{fontWeight: 'bold'}}>Total is {sum}</p>
+    const sum = parts.reduce((total, part) => total + part.exercises, 0);
+    return <p style={{ fontWeight: 'bold' }}>Total is {sum}</p>
 }
 
 const Course = ({ course }) => {
     return(
         <>
             <Header name={course.name} />
-            <div>
+            <>
                 {course.parts.map(part => 
                 <Description key={part.id} text={part.name} exercises={part.exercises} />
                 )}
-            </div>
+            </>
             <Sum parts={course.parts}/>
         </>
     )
