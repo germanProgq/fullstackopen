99f31c92c0dc99cb07722938ef15b4f4dd602470
German <157655511+germanProgq@users.noreply.github.com>
2025-09-07 18:36:03 +0300

    part 2

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,16 +1,55 @@
-import Note from './components/Note'
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
 
-const App = ({ notes }) => {
+const Content = (props) => {
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
   )
 }
+const Total = (props) => {
+  const sum = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises;
+  return <p>Number of exercises {sum} </p>;
+};
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-export default App
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course = {course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default App;
