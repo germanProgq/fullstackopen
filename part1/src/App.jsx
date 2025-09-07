ab48c452634fe101781579499794967c45806c45
German <157655511+germanProgq@users.noreply.github.com>
2025-09-07 11:17:44 +0300

    step 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,11 +6,22 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-    {props.part} {props.exercise}
-    </p>
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+    
   )
 };
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
 const Total = (props) => {
   return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>;
 };
@@ -26,9 +37,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   );
